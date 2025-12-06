a0b48263e94784e64beb295b4e7549f7b7dd051e
Sarthak Raut <sarthak@version-next.com>
2025-12-05 23:21:01 -0500

    Fix favicon aspect ratio

--- src/pages/ProjectChoice.js
+++ src/pages/ProjectChoice.js
@@ -10,9 +10,20 @@ const ProjectChoice = () => {
       <p>
         This system is designed using the 3-Tier Architecture (Frontend, Backend, Database) and handles the core operations of a hotel.
       </p>
+
+      <div className="build-summary">
+        <h3>Team Members</h3>
+        <ul>
+          <li>Sarthak Mohan Raut</li>
+          <li>Praneeth Gunasekaran</li>
+          <li>Pratyaksh Chandra</li>
+          <li>Siddharth Yadav</li>
+          <li>Swarali Satish Kestikar</li>
+          <li>Rudra Prakashkumar Desai</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default ProjectChoice;
-
